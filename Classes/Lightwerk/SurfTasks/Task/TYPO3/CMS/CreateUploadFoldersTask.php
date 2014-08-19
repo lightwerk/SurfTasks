c1a220ad9d0a0b66186333227f54293901669039
Lars Malach <lp@lightwerk.com>
2014-08-19 09:01:32 +0200

    [FEATURE] Adds TYPO3/CMS/CreateUploadFoldersTask

---
+++ Classes/Lightwerk/SurfTasks/Task/TYPO3/CMS/CreateUploadFoldersTask.php
@@ -0,0 +1,56 @@
+<?php
+namespace Lightwerk\SurfTasks\Task\TYPO3\CMS;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lightwerk.SurfTasks".   *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Create upload folder for all installed TYPO3 extensions
+ */
+class CreateUploadFoldersTask extends \TYPO3\Surf\Domain\Model\Task {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * Executes this task
+	 *
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$commands = array();
+		$commands[] = 'cd ' . escapeshellarg($deployment->getApplicationReleasePath($application));
+		if (!empty($options['TYPO3_CONTEXT'])) {
+			$commands[] = 'export TYPO3_CONTEXT=' . escapeshellarg($options['TYPO3_CONTEXT']);
+		}
+		$commands[] = './typo3/cli_dispatch.phpsh extbase extensionapi:createuploadfolders';
+		$this->shell->executeOrSimulate($commands, $node, $deployment);
+	}
+
+	/**
+	 * Simulate this task
+	 *
+	 * @param Node $node
+	 * @param Application $application
+	 * @param Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function simulate(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$this->execute($node, $application, $deployment, $options);
+	}
+}
