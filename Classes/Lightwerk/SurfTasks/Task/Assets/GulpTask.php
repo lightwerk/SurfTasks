45d6b5febf6393a0046e933215fed288cd75c215
Lars Malach <lm@lightwerk.com>
2014-10-31 19:52:01 +0100

    [TASK] Adds GulpTask.php

---
+++ Classes/Lightwerk/SurfTasks/Task/Assets/GulpTask.php
@@ -0,0 +1,79 @@
+<?php
+namespace Lightwerk\SurfTasks\Task\Gulp;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lightwerk.SurfTasks".   *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Task;
+use TYPO3\Surf\Exception\InvalidConfigurationException;
+
+/**
+ * Runs gulp tasks
+ *
+ * @package Lightwerk\SurfTasks
+ */
+class GulpTask extends Task {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * Executes this task
+	 *
+	 * @param Node $node
+	 * @param Application $application
+	 * @param Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 * @throws InvalidConfigurationException
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		if (isset($options['useApplicationWorkspace']) && $options['useApplicationWorkspace'] === TRUE) {
+			$rootPath = $deployment->getWorkspacePath($application);
+		} else {
+			$rootPath = $deployment->getApplicationReleasePath($application);
+		}
+
+		if (!empty($options['nodeName'])) {
+			$node = $deployment->getNode($options['nodeName']);
+			if ($node === NULL) {
+				throw new InvalidConfigurationException(
+					sprintf('Node "%s" not found', $options['nodeName']),
+					1414781227
+				);
+			}
+		}
+
+		$commands = array();
+		$commands[] = 'cd ' . escapeshellarg($rootPath);
+		$commands[] = 'if hash npm 2>/dev/null && hash gulp 2>/dev/null && [ -f gulpfile.js ]; then ' .
+			'npm install && ' .
+			'gulp build ' .
+		'fi;';
+
+		$this->shell->executeOrSimulate($commands, $node, $deployment);
+	}
+
+	/**
+	 * Simulate this task
+	 *
+	 * @param Node $node
+	 * @param Application $application
+	 * @param Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function simulate(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$this->execute($node, $application, $deployment, $options);
+	}
+
+}
