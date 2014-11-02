a029df7131471b81be87a7152f8f852a7f02a379
Lars Malach <lm@lightwerk.com>
2014-11-02 13:41:59 +0100

    [TASK] Configurable sharedPath

--- Classes/Lightwerk/SurfTasks/Task/TYPO3/CMS/SyncSharedTask.php
+++ Classes/Lightwerk/SurfTasks/Task/TYPO3/CMS/SyncSharedTask.php
@@ -51,24 +51,35 @@ class SyncSharedTask extends Task {
 	 * @throws \TYPO3\Surf\Exception\TaskExecutionException
 	 */
 	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		// Get nodes
 		if (empty($options['sourceNode']) || !is_array($options['sourceNode'])) {
 			throw new TaskExecutionException('No sourceNode given in options.', 1409078366);
 		}
 		$sourceNode = $this->nodeFactory->getNodeByArray($options['sourceNode']);
 
-		$relativeSharedPath = $this->getRelativeSharedPathFromSourceNode($sourceNode, $deployment);
+		$localhostNode = $deployment->getNode('localhost');
+
+		// Get shared paths
+		$sourceSharedPath = $this->getSharedPathFromNode($sourceNode, $deployment, $options);
+		if ($sourceSharedPath{0} !== '/') {
+			$sourceSharedPath = rtrim($sourceNode->getOption('deploymentPath'), '/') . '/' . $sourceSharedPath;
+		}
+
 		$localSharedPath = $deployment->getWorkspacePath($application) . '_shared/';
 
-		$localhost = $deployment->getNode('localhost');
+		$destinationSharedPath = $this->getSharedPathFromNode($node, $deployment, $options);
+		if ($destinationSharedPath{0} !== '/') {
+			$destinationSharedPath = rtrim($deployment->getApplicationReleasePath($application), '/') . '/' . $destinationSharedPath;
+		}
 
 		// Sync folder from source to localhost
 		$this->rsyncService->sync(
 			// $sourceNode
 			$sourceNode,
 			// $sourcePath
-			rtrim($sourceNode->getOption('deploymentPath'), '/') . '/' . $relativeSharedPath,
+			$sourceSharedPath,
 			// $destinationNode
-			$localhost,
+			$localhostNode,
 			// $destinationPath
 			$localSharedPath,
 			$deployment,
@@ -78,13 +89,13 @@ class SyncSharedTask extends Task {
 		// Sync folder from localhost to target
 		$this->rsyncService->sync(
 			// $sourceNode
-			$localhost,
+			$localhostNode,
 			// $sourcePath
 			$localSharedPath,
 			// $destinationNode
 			$node,
 			// $destinationPath
-			rtrim($deployment->getApplicationReleasePath($application), '/') . '/' . $relativeSharedPath,
+			$destinationSharedPath,
 			$deployment,
 			$options
 		);
@@ -93,22 +104,32 @@ class SyncSharedTask extends Task {
 	/**
 	 * @param Node $node
 	 * @param Deployment $deployment
+	 * @param array $options
 	 * @return string
 	 * @throws \TYPO3\Surf\Exception\TaskExecutionException
 	 */
-	protected function getRelativeSharedPathFromSourceNode(Node $node, Deployment $deployment) {
-		$commands = array();
-		$commands[] = 'cd ' . escapeshellarg($node->getOption('deploymentPath'));
-		$commands[] = 'readlink ' . escapeshellarg('fileadmin');
-		$output = $this->shell->execute($commands, $node, $deployment, TRUE);
-		if (!preg_match('/(.+)\/fileadmin\/?$/', trim($output), $matches)) {
-			throw new TaskExecutionException('Could not locate fileadmin. Returned value: ' . $output, 1409077056);
+	protected function getSharedPathFromNode(Node $node, Deployment $deployment, $options) {
+		if ($node->hasOption('sharedPath')) {
+			$sharedPath = $node->getOption('sharedPath');
+		} else {
+			if ($node->hasOption('deploymentPath')) {
+				$deploymentPath = $node->getOption('deploymentPath');
+			} elseif (!empty($options['deploymentPath'])) {
+				$deploymentPath = $options['deploymentPath'];
+			} else {
+				throw new TaskExecutionException('No deploymentPath defined!', 1414849872);
+			}
+
+			$commands = array();
+			$commands[] = 'cd ' . escapeshellarg($deploymentPath);
+			$commands[] = 'readlink ' . escapeshellarg('fileadmin');
+			$output = $this->shell->execute($commands, $node, $deployment, TRUE);
+			if (!preg_match('/(.+)\/fileadmin\/?$/', trim($output), $matches)) {
+				throw new TaskExecutionException('Could not locate fileadmin. Returned value: ' . $output, 1409077056);
+			}
+			$sharedPath = $matches[1];
 		}
-		$sharedPath = rtrim($matches[1], '/') . '/';
-		if ($sharedPath{0} === '/') {
-			throw new TaskExecutionException('Absolute shared path found, but relative path expected: ' . $sharedPath, 1414830078);
-		}
-		return $sharedPath;
+		return rtrim($sharedPath, '/') . '/';
 	}
 
 	/**
